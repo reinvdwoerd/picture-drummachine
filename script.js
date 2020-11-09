2f704733cc32215585740efc3d29dab600b849b9
Glitch (hello-webpage) <none>
2020-11-09 13:25:32 +0000

    🕵⚓️ Checkpoint ./script.js:113823/1235 ./index.html:113823/448 ./pose-values.js:113823/22

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const $video = document.querySelector('video')
 const $currentTime = document.querySelector('.current-time')
 const $currentFrame = document.querySelector('.current-frame')
+const $poseData = document.querySelector('.pose-data')
 
 setInterval(() => {
-  $currentTime.innerText = $video = 
+  $currentTime.innerText = $video.currentTime
+  $currentFrame.innerText = Math.floor($video.currentTime * 29.97)
   
-  
+  const posesForCurrentTime = findClosestPoses($video.currentTime)
+  $poseData.innerText = posesForCurrentTime
 }, 16)
+
+
+
+function findClosestPoses(currentTime) {
+  let minDifference = Number.MAX_VALUE
+  let closestPoses = null
+  
+  for (const {time, poses, scores} of poseValues) {
+    const difference = Math.abs(time - currentTime)
+    
+    // It's closer!
+    if (difference < minDifference) {
+      minDifference = difference
+      closestPoses = poses
+    }
+  }
+  
+  return closestPoses
+}
