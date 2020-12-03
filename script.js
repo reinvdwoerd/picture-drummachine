ce1b7d1328efb5974558a68020b36e5aeb87822b
Glitch (hello-webpage) <none>
2020-12-03 13:19:42 +0000

    🎩🛍 Checkpoint ./index.html:113823/202 ./script.js:113823/48108

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ const $currentTime = document.querySelector(".current-time");
 const $currentFrame = document.querySelector(".current-frame");
 const $poseData = document.querySelector(".pose-data");
 
-let poses = [];
-let posesByFrameCache = {}
+let pose = null;
+let posesByFrameCache = {};
 
-function setup() {
+async function setup() {
   let canvas = createCanvas(1920, 1080);
   canvas.parent("main");
 
   // STYLE
-  strokeWeight(5)
-  stroke('white')
-  
+  strokeWeight(5);
+  stroke("white");
+
   // VIDEO
   video = createVideo(
     "https://cdn.glitch.com/fce293e2-7c18-4790-a64f-62ef937bd855%2Fposepose.mp4?v=1606091227303"
@@ -33,55 +33,49 @@ function setup() {
   video.showControls();
   video.hide();
 
-  video.elt.onloadeddata = () => {
-    poseNet = ml5.poseNet(video, () => {
-      console.log("model ready");
-    });
-    
+  video.elt.onloadeddata = async () => {
+    const net = await posenet.load();
 
-    // This sets up an event that fills the global variable "poses"
-    // with an array every time new poses are detected
-    poseNet.on("pose", results => {
-      poses = results;
-      // console.log(poses);
+    setInterval(async () => {
+      const pose = await net.estimateSinglePose(video.elt, {
+        flipHorizontal: false
+      });
+
+      console.log(pose);
 
       // Video position
-      const currentTime = video.elt.currentTime
-      const currentFrame = Math.floor(currentTime * 29.97)
-      $currentTime.innerText = currentTime
-      $currentFrame.innerText = currentFrame
-      
-      posesByFrameCache[currentFrame] = poses
-      
-      
-      for (const result of results) {
-        const { pose } = result;
+      const currentTime = video.elt.currentTime;
+      const currentFrame = Math.floor(currentTime * 29.97);
+      $currentTime.innerText = currentTime;
+      $currentFrame.innerText = currentFrame;
 
-        for (let i = 0; i < pose.keypoints.length; i++) {
-          // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-          const keypoint = pose.keypoints[i]
-          const { part, position } = keypoint;
-          
-          const joint = document.querySelector(
-            `.joint[data-part="${keypoint.part}"]`
-          );
-          const x = clamp(position.x / video.width, 0, 1);
-          const y = clamp(position.y / video.height, 0, 1);
-          
-          // console.log(x, y)
-          
-          if (currentMidiOutput) {
-            currentMidiOutput.sendControlChange(i, x * 127, 1)
-            currentMidiOutput.sendControlChange(i, y * 127, 2)
-          }
-  
-          if (joint) {
-            joint.querySelector(`.x`).innerText = x.toPrecision(2);
-            joint.querySelector(`.progress-x`).value = x.toPrecision(2);
-            joint.querySelector(`.y`).innerText = y.toPrecision(2);
-            joint.querySelector(`.progress-y`).value = y.toPrecision(2);
-          } else {
-            $joints.innerHTML += `
+      posesByFrameCache[currentFrame] = pose;
+
+      for (let i = 0; i < pose.keypoints.length; i++) {
+        // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+        const keypoint = pose.keypoints[i];
+        const { part, position } = keypoint;
+
+        const joint = document.querySelector(
+          `.joint[data-part="${keypoint.part}"]`
+        );
+        const x = position.x
+        const y = position.y
+
+        console.log(x, y)
+
+        if (currentMidiOutput) {
+          currentMidiOutput.sendControlChange(i, x * 127, 1);
+          currentMidiOutput.sendControlChange(i, y * 127, 2);
+        }
+
+        if (joint) {
+          joint.querySelector(`.x`).innerText = x.toPrecision(2);
+          joint.querySelector(`.progress-x`).value = x.toPrecision(2);
+          joint.querySelector(`.y`).innerText = y.toPrecision(2);
+          joint.querySelector(`.progress-y`).value = y.toPrecision(2);
+        } else {
+          $joints.innerHTML += `
               <div class="joint" data-part="${keypoint.part}">
                 <div class="name">${keypoint.part}</div>
 
@@ -100,65 +94,58 @@ function setup() {
                 </div>
              </div>
             `;
-          } 
         }
       }
-    });
+    }, 16);
   };
 
   frameRate(60);
 }
-
-
-
 
 function draw() {
   image(video, 0, 0, width, height);
   // console.log("draw...");
   // We can call both functions to draw all keypoints and the skeletons
   drawKeypoints();
-  drawSkeleton();
+  // drawSkeleton();
 }
 
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
-  // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i++) {
-    // For each pose detected, loop through all the keypoints
-    let pose = poses[i].pose;
+  if (pose) {
     for (let j = 0; j < pose.keypoints.length; j++) {
       // A keypoint is an object describing a body part (like rightArm or leftShoulder)
       let keypoint = pose.keypoints[j];
       // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
-        stroke('black')
-        fill('white')
-        ellipse(keypoint.position.x, keypoint.position.y, 20, 20);
+        stroke("black");
+        fill("white");
+        ellipse(keypoint.position.x * width, keypoint.position.y * height, 20, 20);
       }
     }
   }
 }
 
 // A function to draw the skeletons
-function drawSkeleton() {
-  // Loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i++) {
-    let skeleton = poses[i].skeleton;
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < skeleton.length; j++) {
-      let partA = skeleton[j][0];
-      let partB = skeleton[j][1];
-      stroke('white')
+// function drawSkeleton() {
+//   // Loop through all the skeletons detected
+//   for (let i = 0; i < poses.length; i++) {
+//     let skeleton = poses[i].skeleton;
+//     // For every skeleton, loop through all body connections
+//     for (let j = 0; j < skeleton.length; j++) {
+//       let partA = skeleton[j][0];
+//       let partB = skeleton[j][1];
+//       stroke('white')
 
-      line(
-        partA.position.x,
-        partA.position.y,
-        partB.position.x,
-        partB.position.y
-      );
-    }
-  }
-}
+//       line(
+//         partA.position.x,
+//         partA.position.y,
+//         partB.position.x,
+//         partB.position.y
+//       );
+//     }
+//   }
+// }
 
 // setInterval(() => {
 
@@ -184,16 +171,14 @@ function drawSkeleton() {
 
 // }, 16)
 
-
 $playbackSpeed.oninput = () => {
   video.speed($playbackSpeed.value);
   $playbackSpeedLabel.innerText = $playbackSpeed.value;
 };
 
 $positionSlider.oninput = () => {
-  video.elt.currentTime = video.elt.duration * $positionSlider.value
-}
-
+  video.elt.currentTime = video.elt.duration * $positionSlider.value;
+};
 
 $midiOutputSelect.onchange = () => {
   currentMidiOutput = WebMidi.getOutputByName($midiOutputSelect.value);
@@ -216,7 +201,6 @@ WebMidi.enable(err => {
 
   currentMidiOutput = WebMidi.outputs[0];
 });
-
 
 function clamp(num, min, max) {
   return num <= min ? min : num >= max ? max : num;
