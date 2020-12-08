f31420940ff222d233869651c8a004ad21981f04
Glitch (hello-webpage) <none>
2020-12-08 01:09:07 +0000

    🖖🙋 Checkpoint ./script.js:113823/32658 ./style.css:113823/64 ./index.html:113823/9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* global strokeWeight, stroke, frameRate, posenet, createVideo, createCanvas, fill, ellipse, image, line, width, height, WebMidi */
 let video, net, currentMidiOutput;
 
-const $ = selector => document.querySelector(selector)
+const $ = selector => document.querySelector(selector);
 
 const $playbackSpeed = $(".playback-speed");
 const $playbackSpeedLabel = $(".playback-speed-label");
@@ -16,7 +16,6 @@ const $currentFrame = $(".current-frame");
 const $poseData = $(".pose-data");
 
 let poses = [];
-
 
 async function setup() {
   let canvas = createCanvas(1920, 1080);
@@ -35,7 +34,8 @@ async function setup() {
   stroke("white");
 
   // VIDEO
-  const src = "https://cdn.glitch.com/fce293e2-7c18-4790-a64f-62ef937bd855%2Fposepose.mp4?v=1606091227303";
+  const src =
+    "https://cdn.glitch.com/fce293e2-7c18-4790-a64f-62ef937bd855%2Fposepose.mp4?v=1606091227303";
   video = createVideo(src);
 
   video.volume(0);
@@ -43,11 +43,12 @@ async function setup() {
   video.loop();
   video.showControls();
   video.hide();
-  
-  $positionSlider.setAttribute('max', video.elt.duration)
+
+  $positionSlider.setAttribute("max", video.elt.duration);
 
   net = await posenet.load({
-      // architecture: 'ResNet50',
+    // architecture: 'ResNet50',
+    quantBytes: 4
   });
 
   video.elt.onloadeddata = async () => {
@@ -57,14 +58,13 @@ async function setup() {
   frameRate(30);
 }
 
-
 async function draw() {
   // THE UPDATING ---
   const currentTime = video.elt.currentTime;
   const currentFrame = Math.floor(currentTime * 29.97);
   $currentTime.innerText = currentTime;
   $currentFrame.innerText = currentFrame;
-  $positionSlider.value = currentTime
+  $positionSlider.value = currentTime;
 
   try {
     // Video position
@@ -75,34 +75,24 @@ async function draw() {
       nmsRadius: 20
     });
   } catch (e) {
-    console.log(e)
+    console.log(e);
     return;
   }
 
   // KEYPOINTS =======================================
   // =================================================
   for (let poseI = 0; poseI < poses.length; poseI++) {
-      const pose = poses[poseI];
-    
-      const $pose = $(`.pose[data-pose="${poseI}"]`);
-      
-      if (!$pose) {
-        $joints.innerHTML += `
-          <div class="pose" data-pose="${poseI}">
-            <div class="name">
-              ${poseI}
-            </div>
-          </div>
-        `
-      }
-    
-    
+    const pose = poses[poseI];
+
+    const $pose = $(`.pose[data-pose="${poseI}"]`);
+
+    if ($pose) {
       for (let i = 0; i < pose.keypoints.length; i++) {
         // A keypoint is an object describing a body part (like rightArm or leftShoulder)
         const keypoint = pose.keypoints[i];
         const { part, position } = keypoint;
-        
-        const midiI = poseI * 16 + i
+
+        const midiI = poseI * 16 + i;
 
         // Normalize values ----
         const x = clamp(position.x / video.width, 0, 1);
@@ -114,7 +104,6 @@ async function draw() {
           currentMidiOutput.sendControlChange(midiI, y * 127, 2);
         }
 
-
         // DRAWING ----
         // Draw ellipses over the detected keypoints
         // Only draw an ellipse is the pose probability is bigger than 0.2
@@ -124,52 +113,58 @@ async function draw() {
           ellipse(keypoint.position.x, keypoint.position.y, 20, 20);
         }
 
-
         // UI UPDATING -----
-        const $joint = $pose.querySelector(`.joint[data-part="${keypoint.part}"]`);
+        const $joint = $pose.querySelector(
+          `.joint[data-part="${keypoint.part}"]`
+        );
 
-        if (!$joint) {
+        if ($joint) {
+          // Update display
+          $joint.querySelector(`.x`).innerText = x.toPrecision(2);
+          $joint.querySelector(`.progress-x`).value = x.toPrecision(2);
+          $joint.querySelector(`.y`).innerText = y.toPrecision(2);
+          $joint.querySelector(`.progress-y`).value = y.toPrecision(2);
+        } else {
           $pose.innerHTML += `
-                <div class="joint" data-part="${keypoint.part}" data-pose="${poseI}">
-                  <div class="name">${midiI}: person ${poseI} - ${keypoint.part}</div>
+                  <div class="joint" data-part="${keypoint.part}" data-pose="${poseI}">
+                    <div class="name">${keypoint.part}</div>
 
-                  <div class="grid">
-                    <span class="label">x:</span>
-                    <span class="x"></span>
-                    <progress class="progress-x" min="0" max="1" value="0"></progress>
-                    <button onclick="sendTest(${midiI}, 1)">test</button>
-                  </div>
+                    <div class="grid">
+                      <span class="label">x:</span>
+                      <span class="x"></span>
+                      <progress class="progress-x" min="0" max="1" value="0"></progress>
+                      <button onclick="sendTest(${midiI}, 1)">test</button>
+                    </div>
 
-                  <div class="grid">
-                    <span class="label">y:</span>
-                    <span class="y"></span>
-                    <progress class="progress-y" min="0" max="1" value="0"></progress>
-                    <button onclick="sendTest(${midiI}, 2)">test</button>
-                  </div>
-               </div>
-            `;
+                    <div class="grid">
+                      <span class="label">y:</span>
+                      <span class="y"></span>
+                      <progress class="progress-y" min="0" max="1" value="0"></progress>
+                      <button onclick="sendTest(${midiI}, 2)">test</button>
+                    </div>
+                 </div>
+              `;
         }
-        
-        // Update display
-        $joint.querySelector(`.x`).innerText = x.toPrecision(2);
-        $joint.querySelector(`.progress-x`).value = x.toPrecision(2);
-        $joint.querySelector(`.y`).innerText = y.toPrecision(2);
-        $joint.querySelector(`.progress-y`).value = y.toPrecision(2);
-     }
+      }
+    } else {
+      $joints.innerHTML += `
+          <div class="pose" data-pose="${poseI}">
+            <div class="name">
+              PERSON ${poseI}
+            </div>
+          </div>
+        `;
+    }
   }
 
-  
   // console.log(frameRate())
-  
-  
+
   // THE DRAWING --------
   image(video, 0, 0, width, height);
   // We can call both functions to draw all keypoints and the skeleton
   drawSkeleton();
   drawKeypoints();
 }
-
-
 
 // A function to draw the skeletons
 function drawSkeleton() {
@@ -185,7 +180,7 @@ function drawSkeleton() {
         partB.position.x,
         partB.position.y
       );
-    }  
+    }
   }
 }
 
@@ -199,9 +194,6 @@ function drawKeypoints() {
     }
   }
 }
-
-
-
 
 $playbackSpeed.oninput = () => {
   video.speed($playbackSpeed.value);
@@ -217,12 +209,9 @@ $midiOutputSelect.onchange = () => {
   console.log("changed output to: ", currentMidiOutput.name);
 };
 
-
 function sendTest(i, j) {
   currentMidiOutput.sendControlChange(i, 127, j);
 }
-
-
 
 WebMidi.enable(err => {
   console.log(err);
@@ -237,15 +226,11 @@ WebMidi.enable(err => {
   currentMidiOutput = WebMidi.outputs[0];
 });
 
-
-
 // UTILITIES ===================================
 // =================================================
 function clamp(num, min, max) {
   return num <= min ? min : num >= max ? max : num;
 }
-
-
 
 // DRAG AND DROP ===================================
 // =================================================
@@ -259,10 +244,9 @@ function dropHandler(ev) {
 
   // Load the new video and save the URL
   video.elt.src = URL.createObjectURL(ev.dataTransfer.files[0]);
-  $positionSlider.setAttribute('max', video.elt.duration)
+  $positionSlider.setAttribute("max", video.elt.duration);
   localStorage.setItem("videoSrc", video.elt.src);
-  
-  
+
   if (ev.dataTransfer.items) {
     // Use DataTransferItemList interface to access the file(s)
     for (var i = 0; i < ev.dataTransfer.items.length; i++) {
@@ -282,7 +266,6 @@ function dropHandler(ev) {
     }
   }
 }
-
 
 function dragOverHandler(ev) {
   console.log("File(s) in drop zone");
